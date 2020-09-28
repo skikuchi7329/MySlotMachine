8bca188f9716a7f09e282f89de65a4bbfd0c9469
Shohei Kikuchi <killchance2256@gmail.com>
2020-09-28 17:05:49 +0900

    Add instanse

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 {
   class Panel {
-    constructor(){
+    constructor() {
       const section = document.createElement('section');
       section.classList.add('panel');
 
@@ -10,7 +10,21 @@
       this.img.src = 'img/seven.png';
       
       this.stop = document.createElement('div');
-      
+      this.stop.textContent = 'STOP';
+      this.stop.classList.add('stop');
+
+      section.appendChild(this.img);
+      section.appendChild(this.stop);
+
+      const main = document.querySelector('main');
+      main.appendChild(section);
+
     }
   }
+
+  const panels = [
+    new Panel(),
+    new Panel(),
+    new Panel(),
+  ];
 }
